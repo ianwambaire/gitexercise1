e530675ceb0b8fe3182be82369ba0337d2943f2a
ianwambaire <ian.wambaire@strathmore.edu>
2024-09-30 16:02:14 +0300

    modified mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 function send_welcome_email($to_email, $username) {
-    $subject = "Welcome to ICS 2.2!";
-    $message = "Hello " . $username . ",\n\nThank you for registering.\n\nRegards,\nICS Team";
+    $subject = "Welcome to ICS 2.2! ACCOUNT VERIFICATION";
+    $message = "Hello " . $username . ",\n\nIn order to use this account you need to click here to complete registration process\n\nRegards,\nSystems admin/nICS 2.2";
     $headers = "From: noreply@ics.com";
 
     return mail($to_email, $subject, $message, $headers);
