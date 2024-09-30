e1232cb397a4788e4962fb6335d4a508d267958d
ianwambaire <ian.wambaire@strathmore.edu>
2024-09-30 15:38:37 +0300

    regisration connection

--- db.php
+++ db.php
@@ -4,10 +4,7 @@ $dbname = 'internet';
 $user = 'root';  
 $pass = 'oliviamumbi2010'; 
 try {
-
     $pdo = new PDO("mysql:host=localhost;port=3307;dbname=internet", $user, $pass);
-
-    
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully to the database";
 } catch (PDOException $e) {
