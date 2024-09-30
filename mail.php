ff523c8febf37d718757fa1861b1f6c00924bfdb
ianwambaire <ian.wambaire@strathmore.edu>
2024-09-30 17:27:52 +0300

    added the phpmailer

--- mail.php
+++ mail.php
@@ -10,23 +10,26 @@ function send_welcome_email($to_email, $username) {
     try {
         // Server settings
         $mail->isSMTP();                                           
-        $mail->Host       = 'smtp.example.com';                   
+        $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;                              
-        $mail->Username   = 'your_email@example.com';            
-        $mail->Password   = 'your_password';                   
+        $mail->Username   ='iannganga154@gmail.com'; 
+        $mail->Password   = '##oliviamumbi2010';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;      
         $mail->Port       = 587;                                 
 
-
+        // Recipients
         $mail->setFrom('noreply@ics.com', 'ICS Admin');
-        $mail->addAddress($to_email, $username);              
+        $mail->addAddress($to_email, htmlspecialchars($username));              
+
+        // Content
         $mail->isHTML(true);                                      
         $mail->Subject = "Welcome to ICS 2.2! ACCOUNT VERIFICATION";
-        $mail->Body    = "Hello " . htmlspecialchars($username) . ",<br><br>In order to use this account you need to click <a href='http://example.com/verify?email=$to_email'>here</a> to complete the registration process.<br><br>Regards,<br>Systems admin<br>ICS 2.2";
+        $mail->Body    = "Hello " . htmlspecialchars($username) . ",<br><br>In order to use this account you need to click <a href='http://example.com/verify?email=" . urlencode($to_email) . "'>here</a> to complete the registration process.<br><br>Regards,<br>Systems admin<br>ICS 2.2";
 
         $mail->send();
         return true;
     } catch (Exception $e) {
+        error_log("Mail Error: " . $e->getMessage());  // Log error for debugging
         return false;
     }
 }
